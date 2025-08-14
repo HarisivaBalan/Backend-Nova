54c86c01857fdeb2299a141551bfad6a95b29867
HarisivaBalan <harisivabalanbvmhss@gmail.com>
2025-08-14 05:50:28 +0530

    Added backend code without config.env

---
+++ server.js
@@ -0,0 +1,38 @@
+const app=require('./app');
+const path=require('path');
+const connectDb = require('./config/database');
+const { error } = require('console');
+
+
+connectDb();
+ const server =app.listen(process.env.PORT,()=>
+console.log(`Server listenting to the port ${process.env.PORT} in ${process.env.NODE_ENV}`)
+);
+
+
+//Handling Unhandled Rejection
+process.on('unhandledRejection',(err)=>
+{
+    console.log(`Error:${err.message}`)
+    console.log("Shutting down the server due to unhandled rejection")
+    server.close(()=>
+    {
+        process.exit(1);
+    });
+    
+})
+
+
+process.on('uncaughtException',(err)=>
+{
+    console.log(`Error:${err.message}`)
+    console.log("Shutting down the server due to uncaught exception error")
+    server.close(()=>
+    {
+        process.exit(1);
+    });
+
+})
+module.exports = async (context, req, res) => {
+  res.status(200).send("Hello from Catalyst backend!");
+};
